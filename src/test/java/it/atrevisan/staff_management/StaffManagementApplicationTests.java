4503ef27915b2ca6253bf10e9ef07d81f6c44d91
Andrea Trevisan <atdevel1@gmail.com>
2026-03-02 11:30:46 +0300

    Initial Spring Boot project

---
+++ src/test/java/it/atrevisan/staff_management/StaffManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package it.atrevisan.staff_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
